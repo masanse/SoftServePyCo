ea666ccc0929451a2bf015c865717e91fb79629e
Musiienko Andrii <mus.and.se@gmail.com>
2022-11-03 16:19:33 +0200

    def_2_task_update_1

--- 4_1_quadratic_equation2.py
+++ 4_1_quadratic_equation2.py
@@ -1,6 +1,12 @@
 print('\nРівняння має вигляд: a•x²+b•x+c=0\n')
 
 
+def input_data():
+    arg = []
+    arg += [float(_) for _ in input('Вкажіть послідовно значення аргументів a,b,c через пробіл:\n').split()]
+    return arg
+
+
 def solve_equation(a: float, b: float, c: float):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
@@ -15,12 +21,6 @@ def solve_equation(a: float, b: float, c: float):
         x1 = (-b + discriminant ** (1 / 2)) / (2 * a)
         x2 = (-b - discriminant ** (1 / 2)) / (2 * a)
         print(f'x1 = {x1}\nx2 = {x2}')
-
-
-def input_data():
-    arg = []
-    arg += [float(_) for _ in input('Вкажіть послідовно значення аргументів a,b,c через пробіл:\n').split()]
-    return arg
 
 
 arg = input_data()
