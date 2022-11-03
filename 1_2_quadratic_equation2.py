dc90bec2e69f40133fcaf9a0f6df480492316967
Musiienko Andrii <mus.and.se@gmail.com>
2022-11-03 14:17:46 +0200

    upgrade-1

--- 1_2_quadratic_equation2.py
+++ 1_2_quadratic_equation2.py
@@ -1,7 +1,7 @@
 print('\nРівняння має вигляд: a•x²+b•x+c=0\n')
 
 
-def quadr_solution(a, b, c):
+def solve_equation(a:float, b:float, c:float):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         print('Дискримінант =', discriminant, ",від'ємний, тому у відповіді комплексні числа")
@@ -21,8 +21,8 @@ arg = []
 arg += [float(_) for _ in input('Вкажіть послідовно значення аргументів a,b,c через пробіл:\n').split()]
 
 if len(arg) == 3:
-    quadr_solution(*arg)
+    solve_equation(*arg)
 elif len(arg) == 0:
-    quadr_solution(6, 5, 8)
+    solve_equation(6, 5, 8)
 else:
     print('Щось пішло не так')
