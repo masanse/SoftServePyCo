5af5d4aadf4f04109f39f916be37375b4cdf8835
Musiienko Andrii <mus.and.se@gmail.com>
2022-11-07 02:04:08 +0200

    updates

--- 4_2_Five_numbers.py
+++ 4_2_Five_numbers.py
@@ -1,12 +1,13 @@
+
 def input_data():
     '''Вхідні дані'''
     list_in = [int(_) for _ in input("Вкажіть послідовно п'ять чисел через пробіл: ").split()]
-    return list_in[:5]
+    return list_in[:5]  
 
 
 def search_mm(l: list):
     '''Пошук мінімума та максимума та їх позицій в рядку'''
-    a = min(l);
+    a = min(l)
     b = max(l)
     lmin, lmax = [], []
     for i in range(len(l)):
@@ -18,5 +19,4 @@ def search_mm(l: list):
     return f'minimum: {a}, position: {lmin}\nmaximum: {b}, position {lmax}'
 
 
-Lis = input_data()
-print(search_mm(Lis))
+print(search_mm(input_data()))
